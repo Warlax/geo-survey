2c2a54218722b1c7f5d8fd071a4f56cfa709561f
niznash <niznash@4a643d66-b013-15ba-6647-eaa2b0e05adb>
2010-09-24 22:32:35 +0000

    Added the following workflow: user gets to the site, hits the index.html page, their location is retrieved; if correctly, a question is retrieved from the database (currently this uses fake data but code is mostly there to do the ajax call as explained by Bao).  Then, the code dynamically adds the question and the answers.  The user can then select an answer and press the submit button.  Next, the answer is taken to the code that submits it the database, there's some placeholder code for the ajax function there too.  I call the answer-submitted callback which removes the old question, displays the current question's answer distribution on a map (just calls a function that Andrew needs to create, doesn't really do anything), and retrieves the next question.

--- source/geolocation/geo-callback.js
+++ source/geolocation/geo-callback.js
@@ -9,7 +9,8 @@ Instead, substitute or extend their implementation to do something you want.
 // location has begun; once it's over, the callbackReportLocation() function will be invoked.
 function callbackGettingLocation()
 {
-	var ni = document.getElementById('handle')
+	// add a temporary element to notify the user we are getting the location.
+	var ni = document.getElementById('geolocation-status')
 	var newdiv = document.createElement('div');
 	newdiv.setAttribute('id', 'getting')
 	newdiv.innerHTML = '<p><font color=\'blue\'>Getting Location...</font></p>';
@@ -22,26 +23,48 @@ function callbackGettingLocation()
 // accuracy is in meters
 function callbackReportLocation(latitude, longitude, accuracy)
 {
-	var ni = document.getElementById('handle')
+	// first, remove the element added by callbackGettingLocation...
+	var ni = document.getElementById('geolocation-status')
 	var toRemove = document.getElementById('getting')
 	ni.removeChild(toRemove)
 	
+	// then, add a new element telling the user their location
+	/*
 	var newdiv = document.createElement('div');
 	newdiv.setAttribute('id', 'location')
 	newdiv.innerHTML = '<p><font color=\'green\'>Location: ' + latitude + ', ' + longitude + ' @ ' + accuracy +'</font></p>';
 	ni.appendChild(newdiv);
+	//*/
 	
-	var map = createGoogleMap("map_canvas", latitude, longitude, 20)
-	addMarker(map, latitude, longitude, "You are here", "#0000ff")
+	// populate the hidden latitude/longitude inputs of the form:
+	var latElement = document.getElementById('latitude')
+	var lngElement = document.getElementById('longitude')
+	latElement.value = latitude
+	lngElement.value = longitude
+	
+	//TODO -- get gender and age from user
+	var age = 26
+	var gender = "Male"
+	
+	// populate the hidden latitude/longitude inputs of the form:
+	var genElement = document.getElementById('gender')
+	var ageElement = document.getElementById('age')
+	genElement.value = gender
+	ageElement.value = age
+	
+	// get a random question from the database:
+	getRandomQuestion()
 }
 
 //Called by geolocation.js in response when there was a problem determining the geolocation
 function callbackReportError(errorCode, errorText)
 {
-	var ni = document.getElementById('handle')
+	// first, remove the element added by callbackGettingLocation...
+	var ni = document.getElementById('geolocation-status')
 	var toRemove = document.getElementById('getting')
 	ni.removeChild(toRemove)
 	
+	// then, add a new element telling the user about the error
 	var newdiv = document.createElement('div');
 	newdiv.setAttribute('id', 'location')
 	newdiv.innerHTML = '<p><font color=\'red\'>Error(' + errorCode + '): ' + errorText + '</font></p>';
