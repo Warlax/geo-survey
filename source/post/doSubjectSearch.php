2967930f58711b8a832e53e8521154f97d97414b
vnbao01@gmail.com <vnbao01@gmail.com@4a643d66-b013-15ba-6647-eaa2b0e05adb>
2010-11-06 00:58:27 +0000

    php script to do search by subject that would match any of the query string

---
+++ source/post/doSubjectSearch.php
@@ -0,0 +1,31 @@
+<?php
+
+#Be sure to include the config file for all the params. 
+
+#require_once 'db_config.inc.php';
+require_once 'post.php';
+$queryString = $_POST['queryString'];
+
+#------------------------------------------------------#
+#Start Sql statement, no where yet.
+
+$sql = "Select questionId, questionDesc from Question where questionDesc like '%$queryString%' ";
+
+
+$res = runSQL($sql);
+$result = array();
+while($row =  mysql_fetch_array($res,MYSQL_ASSOC))
+{
+	$result[] = $row;
+}
+
+header("Expires: Mon, 24 Jul 2008 05:00:00 GMT" );
+header("Last-Modified: " . gmdate( "D, d M Y H:i:s" ) . "GMT" );
+header("Cache-Control: no-cache, must-revalidate" );
+header("Pragma: no-cache" );
+
+
+
+echo json_encode($result);
+
+?>
