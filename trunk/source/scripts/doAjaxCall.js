6db840de0f40ce4628765a185558e34e44db48dc
vnbao01@gmail.com <vnbao01@gmail.com@4a643d66-b013-15ba-6647-eaa2b0e05adb>
2010-11-06 00:59:53 +0000

    Add search function doSubjectSearch(callback, queryString) //search by description, any match on the desc will return doCategorySearch(callbac, queryString)//search by category, any match on the category will return NOT FINISH YET

--- trunk/source/scripts/doAjaxCall.js
+++ trunk/source/scripts/doAjaxCall.js
@@ -37,3 +37,69 @@ $.ajax({
       }
     });
 }
+
+//Do a search for all question that has the subject match any of the keyword
+function doSubjectSearch(callback, queryString) {
+$.ajax({
+      type: "POST",
+      url: "../post/doSubjectSearch.php",  //address of the php code
+      data: "queryString="+queryString, // parameter to pass onto the php code.
+      success: function(resp){
+        // we have the response
+        if(resp.indexOf("<!") >0){
+	 			resp = resp.substring(0,resp.indexOf("<!"));
+		}
+
+        callback(JSON.parse(resp)); //callback function is called to handle the resp text.
+        return 1;
+        },
+      error: function(e){
+        callback(0); //callback function need to be able to handle error also.
+        return 0;
+      }
+    });
+}
+
+//Do a search for all question that has the category match any of the keyword
+function doCategorySearch(callback, queryString) {
+$.ajax({
+      type: "POST",
+      url: "../post/doCategorySearch.php",  //address of the php code
+      data: "queryString="+queryString, // parameter to pass onto the php code.
+      success: function(resp){
+        // we have the response
+        if(resp.indexOf("<!") >0){
+	 			resp = resp.substring(0,resp.indexOf("<!"));
+		}
+
+        callback(JSON.parse(resp)); //callback function is called to handle the resp text.
+        return 1;
+        },
+      error: function(e){
+        callback(0); //callback function need to be able to handle error also.
+        return 0;
+      }
+    });
+}
+
+//List all available category in the db.
+function listCategory(callback) {
+$.ajax({
+      type: "POST",
+      url: "../post/listCategory.php",  //address of the php code
+      data: "", // parameter to pass onto the php code.
+      success: function(resp){
+        // we have the response
+        if(resp.indexOf("<!") >0){
+	 			resp = resp.substring(0,resp.indexOf("<!"));
+		}
+
+        callback(JSON.parse(resp)); //callback function is called to handle the resp text.
+        return 1;
+        },
+      error: function(e){
+        callback(0); //callback function need to be able to handle error also.
+        return 0;
+      }
+    });
+}
